783d6d9a5c29a1f02e20b7ad4014a38431b6aac6
Eric Larson <ericlarsonbroom@gmail.com>
2020-08-14 16:47:21 -0600

    Built out endpoints

--- knexfile.js
+++ knexfile.js
@@ -1,18 +1,13 @@
-// Update with your config settings.
-
 module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/publications',
-    connection: {
+    migrations: {
       directory: './db/migrations'
     },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
     useNullAsDefault: true
-    }
-
-
-
-
-
-
+  }
 };
